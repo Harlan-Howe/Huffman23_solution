a2dcbd7ddf5f5c6bf540cd5bb8ef4564ca0f10ed
Harlan Howe <hhowe7@gatech.edu>
2023-09-29 10:30:46 -0500

    Moved from printing to logging. We now have a way to directly compare two encoding trees. Created an expected tree for the first test, rather than an output string. Renamed tests so they are performed in correct order.

--- Test_3_Tree_Tester.py
+++ Test_3_Tree_Tester.py
@@ -97,6 +97,7 @@ class MyTestCase(unittest.TestCase):
         Sample text is the opening chapter of "Foundation" by Isaac Asimov, copyright 1951
         :return:
         """
+        pass
         source_text = """
         HARI SELDON—. . . born in the 11,988th year of the Galactic Era; died 12,069. The dates are more commonly given in terms of the current Foundational Era as -79 to the year 1 F.E. Born to middle-class parents on Helicon, Arcturus sector (where his father, in a legend of doubtful authenticity, was a tobacco grower in the hydroponic plants of the planet), he early showed amazing ability in mathematics. Anecdotes concerning his ability are innumerable, and some are contradictory. At the age of two, he is said to have . . .
 
